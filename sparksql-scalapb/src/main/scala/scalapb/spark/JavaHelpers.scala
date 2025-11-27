6b054143803cbcc90ae8c77d07dfb958b947fcb5
Pierre Ricadat <pierre.ricadat@devsisters.com>
2025-11-27 09:14:00 +0900

    Revert "Polish"
    This reverts commit 1ad29e4219849a4f314cf3e507ee8fe8c7df7b42.

--- sparksql-scalapb/src/main/scala/scalapb/spark/JavaHelpers.scala
+++ sparksql-scalapb/src/main/scala/scalapb/spark/JavaHelpers.scala
@@ -111,7 +111,7 @@ object JavaHelpers {
 
         if (valueDesc.scalaType.isInstanceOf[ScalaType.Message])
           s.iterator.map { m =>
-            (m.getField(keyDesc), m.getField(valueDesc))
+            (m.getField(keyDesc), m.getFieldByNumber(valueDesc.number))
           }
         else
           s.iterator.map { m =>
